fb46439dc1903dc3f25c3a15358976e5a12be2f1
Aikhan Zhashkeyev <spy.04@mail.ru>
2025-08-20 20:07:08 +0500

    chore: add tailwind to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import react from "@vitejs/plugin-react"
-import * as path from "node:path"
-import { defineConfig } from "vitest/config"
-import packageJson from "./package.json" with { type: "json" }
+import react from '@vitejs/plugin-react';
+import * as path from 'node:path';
+import { defineConfig } from 'vitest/config';
+import packageJson from './package.json' with { type: 'json' };
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 
   server: {
     open: true,
@@ -14,15 +15,15 @@ export default defineConfig({
   test: {
     root: import.meta.dirname,
     name: packageJson.name,
-    environment: "jsdom",
+    environment: 'jsdom',
 
     typecheck: {
       enabled: true,
-      tsconfig: path.join(import.meta.dirname, "tsconfig.json"),
+      tsconfig: path.join(import.meta.dirname, 'tsconfig.json'),
     },
 
     globals: true,
     watch: false,
-    setupFiles: ["./src/setupTests.ts"],
+    setupFiles: ['./src/setupTests.ts'],
   },
-})
+});
